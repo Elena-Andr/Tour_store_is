5a93005a44968ae4f9135c58ac50de967997b1c7
Elena-Andr <jopinet@gmail.com>
2016-12-29 23:14:47 +0300

    added method for generating salt

--- src/main/java/ru/innopolis/tourstore/servlets/PasswordAuthentication.java
+++ src/main/java/ru/innopolis/tourstore/servlets/PasswordAuthentication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 /**
  * Helper class for hashing password string
@@ -12,11 +13,17 @@ import java.security.NoSuchAlgorithmException;
 public class PasswordAuthentication {
     private static final Logger LOG = LoggerFactory.getLogger(PasswordAuthentication.class);
 
-    public static byte[] hashPassword(String password, String salt){
+    /**
+     *
+     * @param password
+     * @param salt
+     * @return
+     */
+    public static byte[] hashPassword(String password, byte[] salt){
         byte[] hash = null;
         try{
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            messageDigest.update(salt.getBytes("UTF-8"));
+            messageDigest.update(salt);
             hash = messageDigest.digest(password.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
             LOG.error(e.getMessage(), e);
@@ -24,5 +31,17 @@ public class PasswordAuthentication {
             LOG.error(e.getMessage(), e);
         }
         return hash;
+    }
+
+    /**
+     * Method generates random bytes
+     * @return
+     */
+    public static byte[] generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte salt[] = new byte[20];
+        random.nextBytes(salt);
+
+        return salt;
     }
 }
